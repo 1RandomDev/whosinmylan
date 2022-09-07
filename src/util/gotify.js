eba56715b36809a0c084b5711fa472d5d8cb5566
1RandomDev <84292805+1RandomDev@users.noreply.github.com>
2022-09-07 17:33:23 +0200

    Allow Gotify URLs with trailing slash

--- src/util/gotify.js
+++ src/util/gotify.js
@@ -5,6 +5,10 @@ class Gotify {
         this.url = url;
         this.token = token;
         this.priority = priority;
+
+        if(this.url.endsWith('/')) {
+            this.url = this.url.slice(0, -1);
+        }
     }
 
     sendNotification(title, message) {
